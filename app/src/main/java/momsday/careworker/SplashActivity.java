b3c5f44bf4aff0316a72a077082aefc38d9a76eb
이종현 <dwg7691@gmail.com>
2018-06-11 15:03:53 +0900

    Add Lib

--- app/src/main/java/momsday/careworker/SplashActivity.java
+++ app/src/main/java/momsday/careworker/SplashActivity.java
@@ -3,9 +3,6 @@ package momsday.careworker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,6 +10,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        startActivity(new Intent(this, MainActivity.class));
+        startActivity(new Intent(this, StartActivity.class));
     }
 }
--- app/src/main/java/momsday/careworker/StartActivity.java
+++ app/src/main/java/momsday/careworker/StartActivity.java
@@ -1,15 +1,13 @@
 package momsday.careworker;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
     }
 }
